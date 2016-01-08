e782f00040aab448088423b78e505c81137888a8
Magnus <magnuskollip@hotmail.dk>
2016-01-08 22:16:54 +0100

    færdig buybuilding (ish)

--- src/field/Street.java
+++ src/field/Street.java
@@ -5,30 +5,29 @@ import java.util.Arrays;
 import controllers.GameController;
 import desktop_resources.GUI;
 
-
-
-
 public class Street extends Ownable {
-	private int rents [] = new int [7];
+	private int rents[] = new int[7];
 	private int buildingPrice;
 	private int amountOfHouses;
 	private int maxAmountofHouses;
 	private int streetCategory;
 	private int hotels;
 	private boolean isPawn;
-	public Street(String Titel, String Sub, String Desc, int fieldNo, int price, int pawnPrice,int rents[],int buildingPrice, int streetCategory) {
+
+	public Street(String Titel, String Sub, String Desc, int fieldNo, int price, int pawnPrice, int rents[],
+			int buildingPrice, int streetCategory) {
 		super(Titel, Sub, Desc, fieldNo, price, pawnPrice);
 		this.rents = rents;
 		this.buildingPrice = buildingPrice;
 		this.streetCategory = streetCategory;
 	}
 
-	
 	public boolean buyBuilding (GameController gameController){
+		int streets = this.getAmountOfStreetsInCategory(this.getStreetCategory(), gameController);
 		if (gameController.getPlayerController().getPlayer(gameController.getTurn()-1).getAccount().getBalance() >= buildingPrice){
 	//	String i = GUI.getUserButtonPressed("Do you want to buy a building?", "Yes","No");
 		boolean i = gameController.getGUIController().askYesNoQuestion("Do you want to buy a building");
-		if (i == true && this.isPawn == false && this.getHousesInSection(this.getStreetCategory(), gameController)){
+		if (i == true && this.isPawn == false && this.getamountOfHouses()<=this.getHousesInSection(this.getStreetCategory(), gameController)/streets){
 			if (this.amountOfHouses < maxAmountofHouses){
 			this.amountOfHouses += 1; // Ved ikke helt med denne, vi skal have noget der holder styr på dette.
 			return true;
@@ -39,8 +38,9 @@ public class Street extends Ownable {
 				this.hotels++;
 			}
 			
-			else if 
-			
+			else if (i == false){
+				return false;
+			}
 		}
 		else {
 			return true;
@@ -49,24 +49,23 @@ public class Street extends Ownable {
 		return true;	
 			
 	}
-	public int getamountOfHouses(){
+
+	public int getamountOfHouses() {
 		return this.amountOfHouses;
-		
+
 	}
-	
-	public int getHousesInSection(int i, GameController gameController){
+
+	public int getHousesInSection(int i, GameController gameController) {
 		int houses = 0;
-		for (int j = 0; j<40; j++){
-			
-			if (Street.class == gameController.getFieldController().getFields()[j].getClass()){
-				
-				if (((Street) gameController.getFieldController().getFields()[j]).getStreetCategory() == i){
-					houses += ((Street)gameController.getFieldController().getFields()[j]).getamountOfHouses();
-					
-					
-					
+		for (int j = 0; j < 40; j++) {
+
+			if (Street.class == gameController.getFieldController().getFields()[j].getClass()) {
+
+				if (((Street) gameController.getFieldController().getFields()[j]).getStreetCategory() == i) {
+					houses += ((Street) gameController.getFieldController().getFields()[j]).getamountOfHouses();
+
 				}
-				
+
 			}
 		}
 		return houses;
@@ -76,29 +75,30 @@ public class Street extends Ownable {
 		return rents;
 	}
 
-
 	public void setRents(int[] rents) {
 		this.rents = rents;
 	}
-
 
 	@Override
 	public String toString() {
 		return "Street [rents=" + Arrays.toString(rents) + "]";
 	}
-	
-	public int getStreetCategory(){
+
+	public int getStreetCategory() {
 		return streetCategory;
 	}
-	
-	public int getAmountOfStreetsInCategory(int i, GameController gameController){
+
+	public int getAmountOfStreetsInCategory(int i, GameController gameController) {
 		int count = 0;
-		for (int j = 0; j<40; j++){
-if (Street.class == gameController.getFieldController().getFields()[j].getClass()){
-				
-				if (((Street) gameController.getFieldController().getFields()[j]).getStreetCategory() == i){
+		for (int j = 0; j < 40; j++) {
+			if (Street.class == gameController.getFieldController().getFields()[j].getClass()) {
+
+				if (((Street) gameController.getFieldController().getFields()[j]).getStreetCategory() == i) {
 					count++;
+				}
+			}
+
 		}
+		return count;
 	}
-	
 }
