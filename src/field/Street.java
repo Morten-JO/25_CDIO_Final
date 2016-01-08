8004c52f4093d07ad5fb7facb22874e4cc9749c6
Magnus <magnuskollip@hotmail.dk>
2016-01-08 22:54:31 +0100

    buy og sell building samt andre metoder

--- src/field/Street.java
+++ src/field/Street.java
@@ -22,37 +22,82 @@ public class Street extends Ownable {
 		this.streetCategory = streetCategory;
 	}
 
-	public boolean buyBuilding (GameController gameController){
+	public boolean buyBuilding(GameController gameController) {
+
 		int streets = this.getAmountOfStreetsInCategory(this.getStreetCategory(), gameController);
-		if (gameController.getPlayerController().getPlayer(gameController.getTurn()-1).getAccount().getBalance() >= buildingPrice){
-	//	String i = GUI.getUserButtonPressed("Do you want to buy a building?", "Yes","No");
-		boolean i = gameController.getGUIController().askYesNoQuestion("Do you want to buy a building");
-		if (i == true && this.isPawn == false && this.getamountOfHouses()<=this.getHousesInSection(this.getStreetCategory(), gameController)/streets){
-			if (this.amountOfHouses < maxAmountofHouses){
-			this.amountOfHouses += 1; // Ved ikke helt med denne, vi skal have noget der holder styr på dette.
-			return true;
+		if (gameController.getPlayerController().getPlayer(gameController.getTurn() - 1).getAccount()
+				.getBalance() >= buildingPrice) {
+			// String i = GUI.getUserButtonPressed("Do you want to buy a
+			// building?", "Yes","No"); = Tobias' lortekode
+			boolean i = gameController.getGUIController().askYesNoQuestion("Do you want to buy a building");
+			if (i == true && this.isPawn == false
+					&& this.getamountOfHouses() <= this.getHousesInSection(this.getStreetCategory(), gameController)
+							/ streets) {
+				if (this.amountOfHouses < maxAmountofHouses) {
+					this.amountOfHouses += 1; // Ved ikke helt med denne, vi
+												// skal have noget der holder
+												// styr på dette.
+					return true;
+				}
+
+				else if (this.amountOfHouses == maxAmountofHouses) {
+					this.amountOfHouses = this.amountOfHouses - maxAmountofHouses;
+					this.hotels++;
+				}
+
+				else if (this.hotels > 0) {
+					return false;
+				}
+
+				else if (i == false) {
+					return false;
+				}
+			} else {
+				return true;
 			}
-			
-			else if (this.amountOfHouses == maxAmountofHouses){
-				this.amountOfHouses = this.amountOfHouses-maxAmountofHouses;
-				this.hotels++;
+		}
+
+		else {
+			return false;
+		}
+
+		return true;
+
+	}
+
+	public boolean sellBuilding(GameController gameController) {
+		if (this.owner.equals(gameController.getPlayerController().getCurrentPlayer())) {
+			boolean i = gameController.getGUIController().askYesNoQuestion("Vil du frasælge en bygning?");
+			if (i = true) {
+
+				if (this.getamountOfHouses() > 0) {
+					this.amountOfHouses--;
+					gameController.getPlayerController().getCurrentPlayer().adjustBalance(buildingPrice);
+					return true;
+				} else if (this.getamountOfHotels() > 0) {
+					this.amountOfHouses += 4;
+					this.hotels--;
+					gameController.getPlayerController().getCurrentPlayer().adjustBalance(buildingPrice);
+					return true;
+				}
 			}
-			
-			else if (i == false){
-				return false;
-			}
+			 else if (i == false) {
+					return false;
+				}
 		}
 		else {
-			return true;
+			return false;
 		}
-		}
-		return true;	
-			
+		return false;
 	}
 
 	public int getamountOfHouses() {
 		return this.amountOfHouses;
 
+	}
+
+	public int getamountOfHotels() {
+		return this.hotels;
 	}
 
 	public int getHousesInSection(int i, GameController gameController) {
