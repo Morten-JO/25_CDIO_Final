3c2c638b464a30197712f2b9e53888cb2595d66d
Alexander <Alexander@10.16.174.81>
2016-01-08 23:34:45 +0100

    v0.2.2 - Added PayOilRaiseCC.java

---
+++ src/chancecards/PayOilRaiseCC.java
@@ -0,0 +1,28 @@
+package chancecards;
+
+import java.awt.Point;
+
+import controllers.FieldController;
+import controllers.GameController;
+import player.Player;
+
+public class PayOilRaiseCC extends PayMoneyCC {
+
+	public PayOilRaiseCC(String description) {
+		super(description, 0);
+	}
+	
+	@Override
+	public boolean drawCardAction(GameController gc){
+		Player currentPlayer = gc.getPlayerController().getCurrentPlayer();
+		
+		//get total amount of houses and hotels owned in a Point(houses, hotels)
+		Point buildingsOwned = FieldController.getBuildingsOwnedByPlayer(currentPlayer);
+		
+		//amount to pay
+		amount = 500*buildingsOwned.x + 2000*buildingsOwned.y;
+		
+		return currentPlayer.adjustBalance(-amount);
+	}
+
+}
