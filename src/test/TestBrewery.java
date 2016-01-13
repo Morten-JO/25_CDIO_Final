11df4609f1be185741b4b507949fae5686b41376
Kristofer mar <s153646@DTU-H91R762.win.dtu.dk>
2016-01-13 14:15:48 +0100

    More test's done in the brewery test class

--- src/test/TestBrewery.java
+++ src/test/TestBrewery.java
@@ -12,19 +12,24 @@ import field.Brewery;
 public class TestBrewery {
 	
 	private GameController gc;
+	private Brewery brewery;
 
 	@Before
 	public void setUp() throws Exception 
 	{
 		gc = new GameController();
 		gc.getGUIController().isInDebugMode = true;
+		brewery = new Brewery("tuborg", "Ejer", "Koster: ", 12, 3000, 1500, new int [] {100,200});
 		String name [] = {"Kristofer", "mar"};
 		gc.getPlayerController().createPlayers(name);
+
 		
 	}
 
 	@After
-	public void tearDown() throws Exception {
+	public void tearDown() throws Exception 
+	{
+	
 	}
 
 	@Test
@@ -37,11 +42,26 @@ public class TestBrewery {
 	}
 	
 	@Test
+	public void testDescription()
+	{
+//		Tester om de givende beskriverlser på gui brettet stemmer overens med de rigtige beskrivelser
+		int [] testArray = new int[]{100,200};
+		assertEquals("Tuborg", gc.getFieldController().getFields()[12].getName());
+		assertEquals("Ejer", brewery.getDescriptionText());
+		assertEquals("Koster: 3000" , brewery.getSubText());
+		assertEquals(12, brewery.getNumber());
+		assertEquals(3000, brewery.getPrice());
+		assertEquals(1500, brewery.getPawnPrice());
+	}
+	
+	@Test
 	public void testGetRent() 
 	{
 //		Tester om spiller 1 betaler 100 i rente hvis han lander på spiler 0's brewery når han kun ejer et 
 		gc.getGUIController().debugModeReturnTypeBoolean = true;
 		((Brewery)gc.getFieldController().getFields()[12]).setOwner(gc.getPlayerController().getPlayer(0));
+		
+		gc.getCup().rollDices();
 		
 		gc.getPlayerController().getPlayer(1).setPosition(12);
 		gc.getPlayerController().setCurrentPlayer(1);
@@ -53,9 +73,12 @@ public class TestBrewery {
 	@Test
 	public void testGetRentWithBothOwned()
 	{
+//		Tester om spiller 1 betaler doublet i rente når en spilelr ejer begge breweryfelter. 
 		gc.getGUIController().debugModeReturnTypeBoolean = true;
 		((Brewery)gc.getFieldController().getFields()[12]).setOwner(gc.getPlayerController().getPlayer(0));
 		((Brewery)gc.getFieldController().getFields()[28]).setOwner(gc.getPlayerController().getPlayer(0));
+		
+		gc.getCup().rollDices();
 		
 		gc.getPlayerController().getPlayer(1).setPosition(12);
 		gc.getPlayerController().setCurrentPlayer(1);
@@ -63,4 +86,6 @@ public class TestBrewery {
 		
 		assertEquals(30000-((Brewery)gc.getFieldController().getFields()[12]).getRent(gc), gc.getPlayerController().getPlayer(1).getBalance());
 	}
+	
+	
 }
