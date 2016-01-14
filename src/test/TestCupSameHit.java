70190062bf012b62f38a1c513641f6cc7ed39c76
kristofer <kristofer@Kristofer-PC>
2016-01-14 15:52:39 +0100

    Test how many times we hit the same number

--- src/test/TestCupSameHit.java
+++ src/test/TestCupSameHit.java
@@ -7,18 +7,21 @@ public class TestCupSameHit {
 	public static void main(String[] args) {
 		
 		Cup cup = new Cup(2,6);
+		Cup cup1 = new Cup(2,6);
 		
-		int testSize = 1000000;
+		
+		int testSize = 1000;
 		int sameHit=0;
 		
-		for (int i=1; i<testSize; i++){
+		for (int i = 0 ; i < testSize; i++){
 			cup.rollDices();
+			cup1.rollDices();
 			if (cup.isSameHit()){
 				sameHit++;
 			}
 		}
 		
-		System.out.println("Same result over "+testSize+" rolls: " +sameHit);
+		System.out.println("Same result over " + testSize + " rolls: " +sameHit);
 	}
 
 }
